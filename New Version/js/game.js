597a0ff584dd20ae2d9758b52be003010047dde1
Rimvydas Zilinskas <rilinskas@yahoo.com>
2018-11-02 21:58:21 +0100

    flipping cards working randomly

--- New Version/js/game.js
+++ New Version/js/game.js
@@ -1,23 +1,49 @@
 var rotated = false;
+var reversed = 0;
+const FLIP_INTERVAL = 3000;
 
 $(document).ready(() => {
+
+  //initialize randomly
+  init();
+
   setInterval(()=>{
-    var deg = 180;
-    if(rotated){
-      deg = 0;
-      rotated = false;
-    } else {
-      rotated = true;
-    }
+    // randomize ammount of cards to flip
+    var cardsToFlip = Math.floor(Math.random() * 5 + 1);
+    var flipped = 0;
+
     $(".sdg-img").each((index, element) => {
-      if(index % 2 == 0)
+      if(Math.floor(Math.random() * 2) === 1 && flipped < cardsToFlip){
         $(element).toggleClass("inverted");
+        flipped++;
+      } else if(flipped >= cardsToFlip){
+        return false;
+      }
     });
 
-
-  }, 3000);
+  }, FLIP_INTERVAL);
 });
 
-$(".sdg-img").on("click", e=>{
+function init(){
+  $(".sdg-img").each((index, element) => {
+
+    if(reversed >= Math.floor($(".sdg-img").length * 0.6)){
+      // do not continue to reverse if more than 60% of all are reversed
+      return false;
+    }
+
+    //get a random 0 or 1 for reversed/not reversed
+    var random = Math.floor(Math.random() * 2);
+
+    if(random === 1){
+      // make the element reversed
+      reversed++;
+      $(element).toggleClass("inverted");
+    }
+  });
+}
+
+$(".sdg-img").on("click", (e)=>{
+  // TODO: load appropriate level
   console.log($(e.target).attr("id"));
 });
